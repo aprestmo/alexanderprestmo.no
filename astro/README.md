86206bbf01822079a93cba6c89fcbc0a1f6e3f3f
Alexander Prestmo <aprestmo@gmail.com>
2023-03-28 08:05:38 +0200

    Init

---
+++ astro/README.md
@@ -0,0 +1,1 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/b374485d-6640-44ac-97e0-34eca50ae892/deploy-status)](https://app.netlify.com/sites/alexanderprestmo/deploys)
---
+++ sanity/sanity.config.ts
@@ -0,0 +1,20 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'alexanderprestmo',
+
+  projectId: '4i6ggqv9',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+
+  basePath: '/admin',
+})
