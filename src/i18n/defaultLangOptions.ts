da457f2cf85ac5b8a2bef3eeb4c36297b7719bda
Alexander Prestmo <aprestmo@gmail.com>
2024-10-09 21:58:48 +0200

    Add i18n

---
+++ src/i18n/defaultLangOptions.ts
@@ -0,0 +1,14 @@
+export const Languages = [
+  {
+    name: "Norsk Bokmål",
+    value: "nb-NO",
+  },
+  {
+    name: "English",
+    value: "en",
+  },
+] as const;
+
+export const defaultLanguage = "nb-NO";
+
+export type Lang = (typeof Languages)[number]["value"];
