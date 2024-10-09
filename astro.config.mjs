fc6c490b3a63e59bbaf6bae3c185c5fce7b8a68e
Alexander Prestmo <aprestmo@gmail.com>
2024-10-09 22:27:58 +0200

    feat(app): Add language picker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    defaultLocale: 'nb-NO',
+    locales: ['nb-NO', 'en'],
+    // routing: {
+    //   prefixDefaultLocale: true,
+    // }
+  }
+});
